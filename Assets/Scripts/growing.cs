a2b208f78ea0d2f34e883b69924841341412d754
Frank Jiang <frankjiang1208@gmail.com>
2018-08-02 16:19:17 -0400

    temp

---
+++ Assets/Scripts/growing.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class growing : MonoBehaviour {
+
+    Vector3 temp;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        temp = transform.localScale;
+        temp.y += Time.deltaTime;
+        transform.localScale = temp;
+	}
+}
