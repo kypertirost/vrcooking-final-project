3d94205c592af2ad37bbe2569801b48470ff6f3e
kypertirost <liumiao1018@me.com>
2018-08-02 18:08:35 -0400

    add alpha test

---
+++ Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+    public class Player_Controller : NetworkBehaviour
+    {
+
+        public Transform cameraTransform;
+        public Transform cameraContainerTransform;
+        public Transform visorTransform;
+        public Vector3 direction;
+
+
+
+        public override void OnStartLocalPlayer()
+        {
+            GetComponent<Renderer>().material.color = Color.blue;
+            cameraTransform = Camera.main.transform;
+            cameraContainerTransform = cameraTransform.parent;
+            
+            visorTransform = transform.Find("Visor");
+            cameraContainerTransform.position = visorTransform.position;
+            Debug.Log("start local player: visor position = " + visorTransform.position + " camera posn = " + cameraContainerTransform.position);
+        }
+
+        void Update()
+        {
+            if (!isLocalPlayer)
+            {
+                return;
+            }
+
+            // rotate the player to match the camera's rotation (controlled by GoogleVR)
+            Vector3 yrot = cameraTransform.rotation.eulerAngles;
+            // only rotate around y axis
+            yrot.x = 0;
+            yrot.z = 0;
+            transform.eulerAngles = yrot;
+            //        transform.rotation = cameraTransform.rotation;
+
+            // move the camera to match the player's position
+            cameraContainerTransform.position = visorTransform.position;
+
+        }
+
+    }
+
---
+++ Assets/Scripts/TransformtoCamera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformtoCamera : MonoBehaviour {
+    Vector3 pos;
+	// Use this for initialization
+	void Start () {
+        pos = Camera.main.transform.localPosition;
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = pos;
+	}
+}
